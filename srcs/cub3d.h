1e1364c0a5f6495a35a697ddc0628b74f81359eb
小平波琉 <h.kopei.89315167@icloud.com>
2022-09-03 23:16:26 +0900

    sample除いた&Makefile

--- srcs/cub3d.h
+++ srcs/cub3d.h
@@ -49,10 +49,10 @@ size_t ft_strlen(char *str);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char *s1, char *s2);
 char	*get_next_line(int fd);
+
 #define BUFFER_SIZE 100
 #define READ_ERROR -1
 #define END_OF_FILE 0
 #define NOT_EOF 100
-
 
 #endif
