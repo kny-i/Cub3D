bbe2fd3dc023888c565f675cd29ed885491567bc
小平波琉 <h.kopei.89315167@icloud.com>
2022-09-05 03:45:03 +0900

    wip

--- new/src/parser.c
+++ new/src/parser.c
@@ -1,5 +1,16 @@
 #include "cub3d.h"
 
+bool is_map_line(char *line)
+{
+	size_t i = 0;
+	while (ft_isprint(line[i]) == false)
+		i++;
+	if (line[i] == '1')
+		return (true);
+	else
+		return (false);
+}
+
 bool is_valid_map(int fd, t_map *map, size_t *nb_col)
 {
 	char *line;
@@ -8,20 +19,36 @@ bool is_valid_map(int fd, t_map *map, size_t *nb_col)
 		line = get_next_line(fd);
 		if (line == NULL)
 			break ;
-		(*nb_col)++;
+		if (is_map_line(line) == true)
+			(*nb_col)++;
 	}
 	return (true);//ここもっとちゃんとやんないとだめ
 }
-void	parse_direction(t_map *map, char *line)
+
+bool parse_direction(t_map *map, char *line)
 {
 	if (ft_strncmp(line, "NO", 2) == 0)
+	{
 		map->path[north] = xstrdup(line + 3);
+		return (true);
+	}
 	if (ft_strncmp(line, "SO", 2) == 0)
+	{
 		map->path[south] = xstrdup(line + 3);
+		return (true);
+	}
 	if (ft_strncmp(line, "WE", 2) == 0)
+	{
 		map->path[west] = xstrdup(line + 3);
+		return (true);
+	}
 	if (ft_strncmp(line, "EA", 2) == 0)
+	{
 		map->path[east] = xstrdup(line + 3);
+		return (true);
+	}
+	else
+		return (false);
 }
 
 int allocate_color(t_map *map, char *line)
@@ -36,19 +63,57 @@ int allocate_color(t_map *map, char *line)
 	return (rgb[0] + rgb[1] + rgb[2]);
 }
 
-void parse_color(t_map *map, char *line)
+bool parse_color(t_map *map, char *line)
 {
 	if (ft_strncmp(line, "F", 1) == 0)
+	{
 		map->floor_color = allocate_color(map, line + 2);
+		return (true);
+	}
 	if (ft_strncmp(line, "C", 1) == 0)
+	{
 		map->ceiling_color = allocate_color(map, line + 2);
+		return (true);
+	}
+	else
+		return (false);
 }
 
-void allocate_map(t_map *map, char *line, size_t row)
+bool is_map_line(char *line)
 {
-	parse_direction(map, line);
-	parse_color(map, line);
-	//parse_map();
+	size_t i = 0;
+	while (ft_isprint(line[i]) == false)
+		i++;
+	if (line[i] == '1')
+		return (true);
+	else
+		return (false);
+}
+
+void allocate_map(t_map *map, char *line, size_t *map_row_index)
+{
+	char **strs = malloc(sizeof(char *));
+}
+
+bool parse_map(t_map *map, char *line, size_t *map_row_index)
+{
+	if (is_map_line(line) == false)
+		return (false);
+	else
+	{
+		allocate_map(map, line, map_row_index);
+		return (true);
+	}
+}
+
+void parse_cub3d_file(t_map *map, char *line, size_t *map_row_index)
+{
+	if (parse_direction(map, line)
+	|| parse_color(map, line)
+	|| parse_map(map, line, map_row_index))
+		return;
+	else
+		return;//error handling
 }
 
 void parser(char *file, t_map *map)
@@ -66,16 +131,17 @@ void parser(char *file, t_map *map)
 	close(fd);
 
 	int fd2 = open(file, O_RDONLY);
-	size_t row = 0;
 	map = ft_calloc(1, sizeof(t_map));
-	map->grid = ft_calloc(nb_col, sizeof(char *));
+
+	size_t map_row_index = 0;
 	while (true)
 	{
 		line = get_next_line(fd2);
 		if (line == NULL)
 			break ;
-		allocate_map(map, line, row);
-		row++;
+		parse_cub3d_file(map, line, &map_row_index);
+		free(line);
 	}
+	free(line);
 	debug_parser(map);
 }
