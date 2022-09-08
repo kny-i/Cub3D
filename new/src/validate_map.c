0663bf8c459692c8d71b8e739d96c9690b505ad6
kny-i <kny_i@keio.jp>
2022-09-08 10:00:41 +0900

    wip

--- new/src/validate_map.c
+++ new/src/validate_map.c
@@ -3,7 +3,7 @@
 void set_player_info(t_map *map, size_t x, size_t y, char direction)
 {
 	if (map->is_filled_start_position == true)
-		error_message("invalid map: multiple player");
+		error_message("INVALID MAP: MULTIPLE PLAYER");
 	map->start_position = ft_calloc(1, sizeof (t_point));
 	map->start_position->x = map->start_position->x * TILE_SIZE + 1;
 	map->start_position->y = map->start_position->y * TILE_SIZE + 1;
@@ -18,20 +18,7 @@ void set_player_info(t_map *map, size_t x, size_t y, char direction)
 	map->grid[y][x] = '0';
 	map->is_filled_start_position = true;
 }
-bool xis_specific_char(char c, char *str)
-{
-	size_t	i;
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (c == str[i])
-		{
-			return (true);
-		}
-		i++;
-	}
-	return (false);
-}
+
 void set_player_info_loop(t_map *map)
 {
 	size_t y;
@@ -43,10 +30,8 @@ void set_player_info_loop(t_map *map)
 	{
 		while (map->grid[y][x] != '\0')
 		{
-			if (xis_specific_char(map->grid[y][x], "NSEW") == true)
-			{
+			if (is_specific_char(map->grid[y][x], "NSEW") == true)
 				set_player_info(map, x, y, map->grid[y][x]);
-			}				
 			x++;
 		}
 		x = 0;
