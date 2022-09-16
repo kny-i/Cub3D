887a15eb70822e38db4c796060872b199b87fe9b
kny-i <kny_i@keio.jp>
2022-09-16 16:02:20 +0900

    wip

--- src/utils.c
+++ src/utils.c
@@ -1,15 +1,15 @@
 #include "cub3d.h"
 
-int ft_isspace(int c)
+int	ft_isspace(int c)
 {
 	return (c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == ' ');
 }
 
 char	*xstrdup(const char *s1)
 {
-	char *ret;
+	char	*ret;
 	size_t	len;
-	size_t i;
+	size_t	i;
 
 	ret = ft_calloc(ft_strlen(s1) + 1, sizeof(char));
 	i = 0;
@@ -21,23 +21,24 @@ char	*xstrdup(const char *s1)
 	return (ret);
 }
 
-int usage(void)
+int	usage(void)
 {
-	ft_putstr_fd("[USAGE]\n"
-				 "./cub3d *.cub\n", STDERR_FILENO);
+	ft_putstr_fd("[USAGE]\n./cub3d *.cub\n", STDERR_FILENO);
 	return (FAILURE);
 }
 
-char *skip_space_str(char *str)
+char	*skip_space_str(char *str)
 {
 	while (ft_isspace(*str) == true)
 		str++;
 	return (str);
 }
 
-size_t strs_len(char **strs)
+size_t	strs_len(char **strs)
 {
-	size_t len = 0;
+	size_t	len;
+
+	len = 0;
 	while (strs[len])
 		len++;
 	return (len);
@@ -47,4 +48,3 @@ double distance_to_btw_points(double x0, double y0, double x1, double y1)
 {
 	return (sqrt((x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0)));
 }
-
