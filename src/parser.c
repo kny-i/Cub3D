1e1335c447bd52ef16850111f0252317da4bf0f1
kny-i <kny_i@keio.jp>
2022-09-16 15:11:48 +0900

    wip

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@ void	get_nb_col(int fd, t_map *map, size_t *nb_col)
 
 	while (true)
 	{
-		{line = get_next_line(fd);
+		line = get_next_line(fd);
 		if (line == NULL)
 			break ;
 		if (is_all_strs_space(line) == true)
@@ -18,7 +18,7 @@ void	get_nb_col(int fd, t_map *map, size_t *nb_col)
 	}
 }
 
-bool parst_direction(t_map *map, char *line)
+bool	parst_direction(t_map *map, char *line)
 {
 	if (ft_strncmp(line, "NO", 2) == 0)
 	{
@@ -44,7 +44,7 @@ bool parst_direction(t_map *map, char *line)
 		return (FAILURE);
 }
 
-bool parse_map(t_map *map, char *line, size_t *map_col_index)
+bool	parse_map(t_map *map, char *line, size_t *map_col_index)
 {
 	if (is_all_specific_char(line, "NSEW 01") == false)
 		return (FAILURE);
@@ -58,43 +58,41 @@ bool parse_map(t_map *map, char *line, size_t *map_col_index)
 void parse_cub3d_file(t_map *map, char *line, size_t *map_col_index)
 {
 	if (parst_direction(map, line) == SUCCESS \
-	|| parse_color(map, line) == SUCCESS
+	|| parse_color(map, line) == SUCCESS \
 	|| parse_map(map, line, map_col_index) == SUCCESS)
-		return;
+		return ;
 	else
-		return;
+		return ;
 }
 
-t_cub3d *parser(char *file, t_cub3d *info)
+t_cub3d	*parser(char *file, t_cub3d *info)
 {
-	/* first fd */
-	int fd;
-	char *line;
+	int		fd;
+	char	*line;
+	size_t	nb_col;
+	int		fd2;
+	size_t map_col_index;
 
 	if (is_valid_format_file(file) == false)
 		error_message("INVALID FORMAT FILE!");
 	fd = open(file, O_RDONLY);
 	if (fd < 0)
 		error_message("OPEN FAILURE!");
-
-	size_t nb_col = 0;
-
+	nb_col = 0;
 	get_nb_col(fd, info->map, &nb_col);
 	close(fd);
-
-	/* second fd */
-	int fd2 = open(file, O_RDONLY);
+	fd2 = open(file, O_RDONLY);
 	info->map = ft_calloc(1, sizeof(t_map));
 	info->map->is_filled_start_position = false;
 	info->map->grid = ft_calloc(info->map->nb_col + 1, sizeof(char *));
-	size_t map_col_index = 0;
+	map_col_index = 0;
 	while (true)
 	{
 		line = get_next_line(fd2);
 		if (line == NULL)
 			break ;
 		if (is_all_strs_space(line) == true)
-			continue;
+			continue ;
 		parse_cub3d_file(info->map, line, &map_col_index);
 		free(line);
 	}
