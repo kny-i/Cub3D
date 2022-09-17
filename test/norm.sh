7c63792e111a4f115ed4f3e01a1e0ca32571b712
小平波琉 <h.kopei.89315167@icloud.com>
2022-09-17 14:01:25 +0900

    wip

--- test/norm.sh
+++ test/norm.sh
@@ -4,11 +4,19 @@ red='\033[0;31m'
 green='\033[32m'
 no='\033[0m'
 
-norminette ../src/*.c
+norminette ../src/*.c >/dev/null
 exit_status=$?
 if [ $exit_status -eq 0 ]; then
   echo -e $green"*.c file in srcs are norm ok!"$no
 else
   echo -e $red"*.c file in srcs are norm error!"$no
+fi
+
+norminette ../libft/*.c > /dev/null
+exit_status=$?
+if [ $exit_status -eq 0 ]; then
+  echo -e $green"*.c file in libft are norm ok!"$no
+else
+  echo -e $red"*.c file in libft are norm error!"$no
 fi
 
