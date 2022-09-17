573698899b8267578f41a4aa15312743357d85b9
小平波琉 <h.kopei.89315167@icloud.com>
2022-09-17 14:04:56 +0900

    wip

--- test/norm.sh
+++ test/norm.sh
@@ -7,16 +7,23 @@ no='\033[0m'
 norminette ../src/*.c >/dev/null
 exit_status=$?
 if [ $exit_status -eq 0 ]; then
-  echo -e $green"*.c file in srcs are norm ok!"$no
+  echo -e $green".c file in srcs are norm ok!!!"$no
 else
-  echo -e $red"*.c file in srcs are norm error!"$no
+  echo -e $red".c file in srcs are norm error↓"$no
+fi
+
+norminette ../src/*.h > /dev/null
+exit_status=$?
+if [ $exit_status -eq 0 ]; then
+  echo -e $green"header file in libft are norm ok!!!"$no
+else
+  echo -e $red"header file in libft are norm error↓"$no
 fi
 
 norminette ../libft/*.c > /dev/null
 exit_status=$?
 if [ $exit_status -eq 0 ]; then
-  echo -e $green"*.c file in libft are norm ok!"$no
+  echo -e $green"*.c file in libft are norm ok!!!"$no
 else
-  echo -e $red"*.c file in libft are norm error!"$no
+  echo -e $red"*.c file in libft are norm error↓"$no
 fi
-
