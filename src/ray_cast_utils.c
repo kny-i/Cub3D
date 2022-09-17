1831a34eebcc51af2bd499b76e24b3a587624b0f
小平波琉 <h.kopei.89315167@icloud.com>
2022-09-18 05:27:59 +0900

    方角の情報に関しては全てが出た

--- src/ray_cast_utils.c
+++ src/ray_cast_utils.c
@@ -13,14 +13,11 @@ double	normalize_angle(double ray_angle)
 void	set_ray_direction(t_ray *ray, double ray_angle)
 {
 	ray->angle = normalize_angle(ray_angle);
-	if (M_PI < ray->angle && ray->angle < M_PI * 2)
-		ray->is_face_up = true;
-	if (ray->is_face_up == false)
-		ray->is_face_down = true;
-	if (M_PI / 2 < ray->angle && ray->angle < 3 * M_PI / 2)
-		ray->is_face_left = true;
-	if (ray->is_face_left == false)
-		ray->is_face_right = true;
+	printf("ray->angle:%lf\n", ray->angle);
+	ray->is_face_down = ray_angle > 0 && ray->angle < M_PI;
+	ray->is_face_up = !ray->is_face_down;
+	ray->is_face_right = ray->angle < M_PI / 2 || 3 * M_PI / 2 < ray->angle;
+	ray->is_face_left = !ray->is_face_right;
 }
 
 static double	pythagorean_theorem_for_delta(t_point *light_source, \
@@ -63,15 +60,28 @@ void	get_hit_wall_direction(t_ray *ray)
 	if (ray->horizontal_or_vertical == VERTICAL)
 	{
 		if (ray->is_face_left)
+		{
+//			printf("west\n");
 			ray->wall_direction = west;
+		}
 		else
+		{
+//			printf("east\n");
 			ray->wall_direction = east;
+		}
 	}
 	else
 	{
 		if (ray->is_face_up)
+		{
+//			printf("north\n");
 			ray->wall_direction = north;
+
+		}
 		else
+		{
 			ray->wall_direction = south;
+//			printf("south\n");
+		}
 	}
 }
