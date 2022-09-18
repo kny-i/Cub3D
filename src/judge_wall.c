676a44cf652c293df109694f4e662821b0fce2b8
小平波琉 <h.kopei.89315167@icloud.com>
2022-09-19 08:05:30 +0900

    wip

--- src/judge_wall.c
+++ src/judge_wall.c
@@ -1,6 +1,6 @@
 #include "cub3d.h"
 
-bool	s_wall_conflict(t_cub3d *info, t_point *point)
+bool	is_wall_conflict(t_cub3d *info, t_point *point)
 {
 	if ((point->x < 0.0 || \
 	(double)info->map->nb_row * TILE_SIZE < point->x || \
